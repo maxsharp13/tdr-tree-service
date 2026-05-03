5e76bc7cb8f30fc119835e553076f06dbf860b89
Max Sharpnack <Maxsharpnack@gmail.com>
2026-05-03 13:08:11 -0500

    Fix layout nesting issue causing scroll gap

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Navbar from "../components/Navbar";
 import Hero from "../sections/Hero";
 import Services from "../sections/Services";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
+    <>
       <Hero />
       <Services />
-    </main>
+    </>
   );
 }
